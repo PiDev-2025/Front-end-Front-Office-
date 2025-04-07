2ff3d8cf684c9d65c4f499f5f399d8167d12cbf6
Damien QazarCloud <damien@qazar.cloud>
2025-04-07 10:43:10 +0200

    checkpooint before switching to eden

--- eden.ts
+++
@@ -1,5 +0,0 @@
-// import type { App } from "./api-elysia/src";
-
-// import { createEdenTreatyReactQuery } from "@ap0nia/eden-react-query";
-// console.log("eden");
-// export const eden = createEdenTreatyReactQuery<App>();
